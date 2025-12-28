15a2c6b08892afecffa774caaf998fb3e37797b2
kelly brevin <kellybrevin83@gmail.com>
2025-12-28 13:27:27 +0300

    add t cart logic

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 //OOP TO HANDLE BUTTON CLICKS
 //user clicks add to cart. Intitial count of one is pushed to an empty products array containing product data
-//quantity button appears to 
+//quantity button appears to
+
+//class to handle the cart count
+class CartCounter {
+  constructor() {
+    this.counts = {};
+  }
+
+  addArray(array) {
+    array.forEach((item) => {
+      this.counts[item] = (this.counts[item] ?? 0) + 1;
+    });
+  }
+  addItem(item) {
+    this.counts[item] = (this.counts[item] ?? 0) + 1;
+  }
+  getAllCounts() {
+    return this.counts;
+  }
+}
