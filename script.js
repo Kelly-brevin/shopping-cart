a1812197dc833500f2655fde4d603560ee22fe78
kelly brevin <kellybrevin83@gmail.com>
2025-12-29 21:23:33 +0300

    updated add to cart logic

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ class CartCounter {
     this.counts[item] = (this.counts[item] ?? 0) + 1;
   }
   getAllCounts() {
-    return this.counts;
+    return Object.values(this.counts).reduce((sum, count) => sum + count, 0);
   }
 }
+
 const counter = new CartCounter();
 
 //Grab DOM elements
@@ -25,8 +26,11 @@ const currentCount = document.getElementById("cartCount");
 //event listener
 addCart.addEventListener("click", () => {
   //add a specific item
-  counter.addItem("");
+  counter.addItem("apple");
 
   //update the DOM with the new count
-  currentCount.textContent = counter.getAllCounts()["20L water bottle"];
+  currentCount.innerText = `cart:${counter.getAllCounts()}`;
 });
+
+//textContent returns all the text exactly as it appears in the DOM(including hidden text and spacing)
+//.innerText returns only the rendered text that is visible to the user, respecting CSS styling
